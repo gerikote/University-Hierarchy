572423eadcf1e69c2fcde59ff481d8f2a498b426
gerikote <gkote.laba@solvd.com>
2023-04-15 22:49:38 -0400

    Added grades functionality and study method

--- Task2/Demo.java
+++ Task2/Demo.java
@@ -9,16 +9,20 @@ public class Demo {
         GraduateStudents graduate2 = new GraduateStudents("2628", "Maria", "Foss", "mfos@gmail.com", "Math", "Complex number analysis");
         GraduateStudents graduate3 = new GraduateStudents("2654", "Ryan", "Creed", "rcreed@gmail.com", "Engineer", "Structural mechanics");
 
+        //Graduate 1 introducing himself
+        System.out.println();
         graduate1.introduceMyself();
-
 
         //Creating Undergraduate Students
         UndergraduateStudents undergraduate1 = new UndergraduateStudents("5647", "Diego", "Gonazalez", "dgonzalez@gmail.com", "Business", 1);
         UndergraduateStudents undergraduate2 = new UndergraduateStudents("2569", "Ivan", "Gonazalez", "Igonzalez@gmail.com", "Medical", 2);
         UndergraduateStudents undergraduate3 = new UndergraduateStudents("1238", "Chris", "White", "cwhite@gmail.com", "Physics", 4);
 
+        //Undergraduate 1 introducing himself
+        System.out.println();
         undergraduate1.introduceMyself();
 
+        //Creating an ArrayList of students to enter as an input at University
         ArrayList<Students> studentsList = new ArrayList<>();
         studentsList.add(graduate1);
         studentsList.add(graduate2);
@@ -27,26 +31,28 @@ public class Demo {
         studentsList.add(undergraduate2);
         studentsList.add(undergraduate3);
 
-
         //Creating courses
         Course biology101 = new Course("101", "Biology101", null, 25);
+        Course math101 = new Course("101", "Math101", null, 25);
+
         String[] biology102prerequisites = {"biology101"};
         Course biology102 = new Course("102", "Biology102", biology102prerequisites, 25);
-        Course math101 = new Course("101", "Math101", null, 25);
 
         String[] math102prerequisites = {"biology101"};
         Course math102 = new Course("102", "Math102", biology102prerequisites, 25);
 
         //Courses arrays
         Course[] bio = {biology101, biology102};
+        Course[] mathematics = {math101, math102};
 
         //Creating Academic Staff
         AcademicStaff professor1 = new AcademicStaff("1", "Clayton", "Tumblerson", "ctamp@gmail.com", bio);
         AcademicStaff professor2 = new AcademicStaff("2", "Tom", "Mullins", "tmullins@gmail.com", null);
         AcademicStaff professor3 = new AcademicStaff("3", "Andrew", "Richards", "arichards@gmail.com", null);
 
+        //Professor 1 introducing himself
+        System.out.println();
         professor1.introduceMyself();
-
 
         //Creating administrative Staff
         AdministrativeStaff adminStaff1 = new AdministrativeStaff("10", "Tara", "Flowers", "tflowers@gmail.com", null);
@@ -55,6 +61,7 @@ public class Demo {
         AdministrativeStaff labAs1 = new AdministrativeStaff("13", "Carl", "Wig", "carl@gmail.com", null);
         AdministrativeStaff labAs2 = new AdministrativeStaff("14", "Nick", "Wig", "nickw@gmail.com", null);
 
+        //Creating an arrayList for staff to enter as an input at University object
         ArrayList<Staff> staffList = new ArrayList<>();
         staffList.add(professor1);
         staffList.add(professor2);
@@ -65,7 +72,6 @@ public class Demo {
         staffList.add(labAs1);
         staffList.add(labAs2);
 
-
         //Creating lectures
         Lecture math = new Lecture("101", "Math", null, 30, professor1);
         Lecture biology = new Lecture("105", "Biology", null, 30, professor2);
@@ -75,6 +81,8 @@ public class Demo {
         Lab lab1 = new Lab("101", "Physics", null, 10, labAs1);
         Lab lab2 = new Lab("102", "Biology", null, 10, labAs2);
 
+
+        //Creating an ArrayList to enter as an input at University
         ArrayList<Course> courseList = new ArrayList<>();
         courseList.add(math);
         courseList.add(biology);
@@ -84,12 +92,32 @@ public class Demo {
         courseList.add(biology101);
         courseList.add(biology102);
 
-        University NTUA = new University("National Technical Univarsity of Athens", "Athens,Zografu", studentsList, staffList, courseList);
-        /** System.out.println(NTUA.getCourses());
-         *System.out.println(NTUA.getStudents());
-         */System.out.println(NTUA.getStaff());
+        //Creating an object of University class
+        University NTUA = new University("National Technical University of Athens", "Athens,Zografu", studentsList, staffList, courseList);
 
+        //Print the University Details
+        System.out.println();
         NTUA.getDetails();
+
+        Grades grades = new Grades();
+        math.enrollStudent(undergraduate1);
+        biology.enrollStudent(undergraduate1);
+
+        //Check the student's grades
+        System.out.println();
+        System.out.println("These are the grades for undergraduate 1 Diego");
+        System.out.println(undergraduate1.getGrades());
+
+        //Make the student study
+        System.out.println();
+        undergraduate1.study();
+
+        System.out.println("This is the average grade for undergraduate 1 Diego");
+        System.out.println(grades.calculateGrade(undergraduate1));
+        System.out.println();
+
+        System.out.println("These are the grades for undergraduate 1 Diego");
+        System.out.println(undergraduate1.getGrades());
 
     }
 }
